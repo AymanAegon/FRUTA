6c68933cd0d3b6edc0536a284c0445d9be8fc6c7
AymanAegon <ayo12ammar24@gmail.comnode>
2023-09-11 20:04:55 +0100

    more stuff

--- website/views.py
+++ website/views.py
@@ -4,4 +4,12 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", home=True)
+
+@views.route('/orders')
+def orders():
+    return render_template("orders.html", orders=True)
+
+@views.route('/clients')
+def clients():
+    return render_template("clients.html", clients=True)
