e69eaca8b5e4340136b0fac16ceaf8e597a8c697
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2023-09-24 18:27:39 +0100

    orders

--- website/models/order.py
+++ website/models/order.py
@@ -2,17 +2,39 @@
 """
 a class Order that inherits from BaseModel:
 """
-import models
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
+from sqlalchemy.orm import relationship
+from models.client import Client
+from models.product import Product
+from models.box import Box
+from os import getenv
+
+
+
+storage_type = getenv("FRUTA_TYPE_STORAGE")
+
+
+
+
 
 
 class Order(BaseModel):
     """
     Order class
     """
+    __tablename__ = "orders"
+    if storage_type == "db":
 
-    client_id = ''
-    product_id = ''
-    quantity = 0
-    total_price = 0
-    number_box = 0
+        client_id = Column(String(60), ForeignKey("clients.id"), nullable=False)
+        product_id =Column(String(60), ForeignKey("products.id"), nullable=False)
+        quantity = Column(Float,nullable=False)
+        total_price = Column(Float,nullable=False)
+        number_box = Column(Integer,nullable=False)
+    else:
+        client_id = ''
+        product_id = ''
+        quantity = 0
+        total_price = 0
+        number_box = 0
+
