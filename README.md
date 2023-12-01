377a08e973ad9b1fff67c9bb9f2f04be5e4e0daa
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2023-12-01 18:43:33 +0100

    stock fixed

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # FRUTA
-## Usage:
-run: python3 main.py
+FRUTA__MYSQL_USER=fruta_dev FRUTA__MYSQL_PWD=fruta_dev_pwd FRUTA__MYSQL_HOST=localhost FRUTA__MYSQL_DB=fruta_dev_db FRUTA__TYPE_STORAGE=db python3 main.py
--- models/product.py
+++ models/product.py
@@ -14,13 +14,13 @@ class Product(BaseModel, Base):
     """
     Product class
     """
-    __tablename__ = "products"
     if models.StorageType == 'db':
+        __tablename__ = "products"
         name = Column(String(128), nullable=False)
         unit_price = Column(Float, nullable=False)
         unit_name = Column(String(128), nullable=True, default="unit")
-        stock = Column(Float, nullable=False, default=0)
-        orders = relationship('Order', cascade="all,delete", backref="product")
+        stock = Column(String(128), nullable=False, default=0)
+       
     else:
         name = ''
         unit_price = 0
